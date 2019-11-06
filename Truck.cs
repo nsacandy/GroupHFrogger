eb7f240d742e5512fac4a7a3bd3f391a9c379439
nsacandy <56607767+nsacandy@users.noreply.github.com>
2019-11-06 09:02:34 -0500

    Some backend I started making for the factory

---
+++ Truck.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FroggerStarter.View.Sprites;
+
+namespace FroggerStarter.Model
+{
+    class Truck : Vehicle
+    {
+
+        public Truck()
+        {
+            this.vehicleSprite = new TruckSprite();
+
+            this.VehicleDirection = heading;
+            if (heading.Equals(Vehicle.Heading.Right))
+            {
+                this.flipHorizontally();
+            }
+        }
+    }
+}
