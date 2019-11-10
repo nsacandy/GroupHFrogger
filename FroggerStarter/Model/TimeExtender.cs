e4f080529dbac86d0d8f6b2e472b6333fca479eb
Aaron Merrell <AMerrell2@my.westga.edu>
2019-11-09 20:47:04 -0500

    Added a TimeExtender class.

---
+++ FroggerStarter/Model/TimeExtender.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FroggerStarter.Model
+{
+    public class TimeExtender : GameObject
+    {
+
+
+        public TimeExtender()
+        {
+            
+        }
+
+        public override void setHeading(Heading heading)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
