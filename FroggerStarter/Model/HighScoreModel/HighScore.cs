788efcd3ddad9f6f9a16d70ae2e30106800819ea
Aaron Merrell <AMerrell2@my.westga.edu>
2019-11-10 19:00:19 -0500

    Added the classes for the high scoreboard.

---
+++ FroggerStarter/Model/HighScoreModel/HighScore.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FroggerStarter.Model.HighScoreModel
+{
+    class HighScore
+    {
+        public string Name { get; set; }
+        public int Score { get; set; }
+        public int Level { get; set; }
+
+        public HighScore()
+        {
+            
+        }
+
+    }
+}
